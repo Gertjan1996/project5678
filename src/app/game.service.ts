1e3b1dc6d5b3f3ac01cb370409b1560dad0f8f74
Gertjan Haan <gertjanhaan@live.nl>
2019-05-29 00:44:24 +0200

    Shows cards (standard 4x4)

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,10 +4,55 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GameService {
-
+  speelVeld = { "Rows": [] };
+  kaartenArray = [];
   boardsize: number;
+  karakter: string;
 
   constructor() {
     this.boardsize = 4;
+    this.karakter = '#';
+    this.vulSpeelveld()
+  }
+
+  vulSpeelveld(kaartCount = 0) {
+    var getNextLetter = this.nextLetter(this.boardsize);
+
+    for (var rows = 0; rows < this.boardsize; rows++) {
+      this.speelVeld.Rows.push({ kaarten: [] })
+
+      for (var columns = 0; columns < this.boardsize; columns++) {
+        this.speelVeld.Rows[rows].kaarten.push(kaartCount);
+        this.kaartenArray.push(getNextLetter())
+        kaartCount++;
+      }
+    }
+  }
+
+  nextLetter = function (size) {
+    var letterArray = "AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPPQQRRSSTTUUVVWWXXYYZZ".substring(0, size * size).split('');
+    var idx = 0;
+
+    letterArray = this.shuffle(letterArray);
+
+    return function () {
+      var letter = letterArray[idx++];
+      return letter;
+    }
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
 }
