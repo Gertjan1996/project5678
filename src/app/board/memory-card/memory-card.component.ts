6fc207722d663b9d93fd9572baad5b4cae551189
Gertjan Haan <gertjanhaan@live.nl>
2019-05-28 23:58:50 +0200

    service toegevoegd
    en poging tot memory-card-component schrijven

--- src/app/board/memory-card/memory-card.component.ts
+++ src/app/board/memory-card/memory-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../game.service';
 
 @Component({
   selector: 'app-memory-card',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemoryCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private game: GameService) { }
 
   ngOnInit() {
   }
