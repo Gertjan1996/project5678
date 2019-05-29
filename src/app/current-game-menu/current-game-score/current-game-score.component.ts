5a9afb52e16650146c2e3a5bcebbf8ff78502523
Gertjan Haan <gertjanhaan@live.nl>
2019-05-29 03:26:10 +0200

    Border en small stuff

--- src/app/current-game-menu/current-game-score/current-game-score.component.ts
+++ src/app/current-game-menu/current-game-score/current-game-score.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../game.service';
 
 @Component({
   selector: 'app-current-game-score',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentGameScoreComponent implements OnInit {
 
-  constructor() { }
+  constructor(public game: GameService) { }
 
   ngOnInit() {
   }
--- src/app/current-game-menu/current-time-left/current-time-left.component.ts
+++ src/app/current-game-menu/current-time-left/current-time-left.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../game.service';
 
 @Component({
   selector: 'app-current-time-left',
   templateUrl: './current-time-left.component.html',
-  styleUrls: ['./current-time-left.component.css']
-})
+  styleUrls: ['./current-time-left.component.css'],
+ })
 export class CurrentTimeLeftComponent implements OnInit {
 
-  constructor() { }
+  constructor(public game: GameService) { }
 
   ngOnInit() {
   }
